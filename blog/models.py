d625b65d0e82699d920d1f331b9c50d3e7a88373
MafeMCode <marcosfdezmoya@gmail.com>
2024-11-27 11:01:27 +0100

    Pruebas de Querysets

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=60)
     cuerpo = models.TextField()
+    fpublicacion = models.DateField(default='2024-07-22')
     
     def __str__(self):
         return self.titulo
-    
