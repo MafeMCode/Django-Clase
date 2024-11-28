41bfd1088a205e9ee680196003a92281bc8c5cd4
MafeMCode <marcosfdezmoya@gmail.com>
2024-11-28 11:08:10 +0100

    paginas individuales para cada detalle

--- primerproyecto/settings.py
+++ primerproyecto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hgfe%remm-$=8agd^b+_=aj63qk+^1%$#j^u00ae%4z)h)o9%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
